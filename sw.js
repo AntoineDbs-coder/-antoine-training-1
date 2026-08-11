f8b92e9467a4e91aa9a73f37a0daac895774f788
AntoineDbs-coder <antoine.fdub@gmail.com>
2026-08-12 00:49:44 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,29 +1,5 @@
-const VERSION="antoine-training-iphone-v2";
-const CORE=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./apple-touch-icon.png"];
-
-self.addEventListener("install",e=>{
-  e.waitUntil(caches.open(VERSION).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()));
-});
-
-self.addEventListener("activate",e=>{
-  e.waitUntil(
-    caches.keys()
-      .then(keys=>Promise.all(keys.filter(k=>k!==VERSION).map(k=>caches.delete(k))))
-      .then(()=>self.clients.claim())
-  );
-});
-
-self.addEventListener("fetch",e=>{
-  if(e.request.method!=="GET") return;
-  const u=new URL(e.request.url);
-  if(u.origin!==location.origin) return;
-  e.respondWith(
-    fetch(e.request)
-      .then(r=>{
-        const copy=r.clone();
-        caches.open(VERSION).then(c=>c.put(e.request,copy));
-        return r;
-      })
-      .catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html")))
-  );
-});
+const CACHE='antoine-training-v3-20260812';
+const ASSETS=["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png", "./chestPress.jpg", "./chestRow.jpg", "./crunch.jpg", "./facePull.jpg", "./hackSquat.jpg", "./hipThrust.jpg", "./inclineDB.jpg", "./latPulldown.jpg", "./lateral.jpg", "./legCurl.jpg", "./legPress.jpg", "./neutralPull.jpg", "./pallof.jpg", "./plank.jpg", "./rdl.jpg", "./shoulderPress.jpg", "./walkingLunge.jpg"];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const u=new URL(e.request.url);if(u.origin!==location.origin)return;e.respondWith(fetch(e.request).then(r=>{const cp=r.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));});
