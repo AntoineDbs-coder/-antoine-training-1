3bfb2697f1a5c96cdbfb788cb7808774530e4730
AntoineDbs-coder <antoine.fdub@gmail.com>
2026-08-12 01:12:16 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='antoine-training-v3-20260812';
+const CACHE='antoine-training-v31-20260812a';
 const ASSETS=["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png", "./chestPress.jpg", "./chestRow.jpg", "./crunch.jpg", "./facePull.jpg", "./hackSquat.jpg", "./hipThrust.jpg", "./inclineDB.jpg", "./latPulldown.jpg", "./lateral.jpg", "./legCurl.jpg", "./legPress.jpg", "./neutralPull.jpg", "./pallof.jpg", "./plank.jpg", "./rdl.jpg", "./shoulderPress.jpg", "./walkingLunge.jpg"];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
